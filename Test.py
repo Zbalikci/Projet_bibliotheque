1a98811f9c6347a4c0a2ff7a1fbbb10ca7811f11
Zbalikci <114244019+Zbalikci@users.noreply.github.com>
2022-11-26 15:17:43 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 from os import path
 import sys
-
+#################################### TEST POUR TROUVER LA FONCTION ####################################
 dossier ="C:/Users/Zeynep/Downloads/livres"
 
 #print(glob.glob("C:/Users/Zeynep/Downloads/livres/ze*"))
@@ -38,6 +38,37 @@ for doc in documents:
         
 print(len(DocumentsPDF)+len(DocumentsEpub)+len(DocumentsZip)+len(DocumentsAutres)) #doit afficher 4323
 
+####################################       on test le module       ####################################
 from bibli import Trier
 
 print(Trier(dossier))
+
+#################################### TEST POUR TROUVER LA FONCTION ####################################
+from PyPDF2 import PdfReader
+
+myFile="C:/Users/Zeynep/Downloads/livres_2/gide_immoraliste.pdf"
+
+reader = PdfReader(myFile)
+number_of_pages = len(reader.pages)
+page = reader.pages[0]
+text = page.extract_text()
+
+metadata = reader.getDocumentInfo()
+
+author = metadata.author if metadata.author else u'Unknown'
+title = metadata.title if metadata.title else myFile
+print (author + "|" + title )
+
+####################################       on test le module       ####################################
+from bibli import Pdf
+
+livre = Pdf("C:/Users/Zeynep/Downloads/livres_2/gide_immoraliste.pdf")
+print(livre)
+
+####################################       on test le module       ####################################
+
+from bibli import Livres
+
+Files=Trier(dossier).DocumentsPDF
+Books=Livres(Files)
+print(Books)
