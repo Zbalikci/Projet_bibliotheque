e75bd7d8966439108611031e6ac9212a82b47226
mariama24 <114244141+mariama24@users.noreply.github.com>
2022-12-16 19:17:01 +0100

    Update bibli.py

--- bibli.py
+++ bibli.py
@@ -3,19 +3,19 @@ import sys
 from Partie1_Balikci_Mbaye import Rapport
 
 if __name__ == "__main__":
-    if sys.argv[1]="-c":
-        conf=sys.argv[2]
-    
     conf="bibli.conf"
+    if sys.argv[1]=="-c":
+        conf=sys.argv[2]
     f = open(conf, "r")
-    lines=f.readlines()
-    dossier=lines[0]
-    chemin_rapports=lines[1]
 
-    if(sys.argv[-1])=="init":
-        r=Rapport(dossier)
+    lines=f.readlines()
+    dossier=lines[0].replace("\n","")
+    chemin_rapports=lines[1]
+    r=Rapport(dossier)
+
+    if "init" in sys.argv:
         r.write(chemin_rapports)
         r.ToC(chemin_rapports)
 
-    elif(sys.argv[-1])=="update":
+    elif "update" in sys.argv:
         r.MaJ(chemin_rapports)
